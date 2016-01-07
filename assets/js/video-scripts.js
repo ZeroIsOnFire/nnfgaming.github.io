b5d51d12657cb3d98496b8d1ae9a36cde926f5da
Eli Fachin Junior <juniorfachin@hotmail.com>
2016-01-07 15:36:54 -0200

    Atualização da pagina

--- assets/js/video-scripts.js
+++ assets/js/video-scripts.js
@@ -1,6 +1,8 @@
 
 jQuery(document).ready(function() {
 
+    $('.product-showcase').backstretch('assets/img/backgrounds/background2.jpg');  
+
     /*
         Gallery
     */
@@ -65,16 +67,61 @@ jQuery(document).ready(function() {
     var parallax = document.querySelectorAll(".parallaxbg"),
       speed = 0.5;
 
-  window.onscroll = function(){
-    [].slice.call(parallax).forEach(function(el,i){
 
-      var windowYOffset = window.pageYOffset,
-          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+    $(window).scroll(function () {
+        [].slice.call(parallax).forEach(function(el,i){
 
-      el.style.backgroundPosition = elBackgrounPos;
+            var windowYOffset = window.pageYOffset,
+                elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+            el.style.backgroundPosition = elBackgrounPos;
+
+        });
+
+        //Display or hide scroll to top button 
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
 
     });
-  };
+
+    if ($(window).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+
+    /*
+    Function for scroliing to top
+    ************************************/
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+        
+    $('.links a[href*=#]').each(function () {
+        if (filterPath(location.pathname) == filterPath(this.pathname) && location.hostname == this.hostname && this.hash.replace(/#/, '')) {
+            var $targetId = $(this.hash),
+            $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
+            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+
+            if ($target) {
+
+                $(this).click(function () {
+                   var targetOffset = $target.offset().top - 63;
+                    $('html, body').animate({
+                        scrollTop: targetOffset
+                    }, 800);
+                    return false;
+                });
+            }
+        }
+    });
 
 });
 
@@ -82,6 +129,10 @@ function openPopup(url)
 {
     newwindow=window.open(url,'name','height=300,width=500');
     if (window.focus) {newwindow.focus()}
+}
+
+function filterPath(string) {
+    return string.replace(/^\//, '').replace(/(index|default).[a-zA-Z]{3,4}$/, '').replace(/\/$/, '');
 }
 
 
